309bcb0d37ce412b641917547d439d955cb7ffa6
Your Name <you@example.com>
2025-06-06 17:45:33 +0200

    implement form for ticket price calculation with user inputs

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,16 @@ MILESTONE 1:
 Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console. 
 */
 
-const btn = document.getElementById('calcPrice');
+/* MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
 
-btn.addEventListener('click', function () {
+
+const form = document.getElementById('ticketForm');
+
+form.addEventListener('submit', function (e) {
+
+    e.preventDefault()
+
     const userKm = Number(document.getElementById('kmInput').value);
     const userAge = Number(document.getElementById('ageInput').value);
     let fullPrice = 0.21 * userKm
@@ -23,3 +30,4 @@ btn.addEventListener('click', function () {
     console.log('Il prezzo del tuo biglietto è', fullPrice.toFixed(2), '€')
 
 })
+
