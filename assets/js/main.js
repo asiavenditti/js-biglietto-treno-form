e96a2dfc5b76a6527e2b8a2d894ee670c4d45868
Your Name <you@example.com>
2025-06-09 11:21:52 +0200

    Refactor ticket form submission logic and improve variable handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,30 @@ Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente
 /* MILESTONE 2:
 Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
 
+const ticketType = document.getElementById('ticketType')
+const newTicket = document.getElementById('generateTicket');
+const nameTicket = document.getElementById('passengerName');
+const totalPrice = document.getElementById('ticketPrice');
+const userKmEl = document.getElementById('kmInput');
+const userAgeEl = document.getElementById('ageSelect');
+const nameValueEl = document.getElementById('nameInput');
+
+
+// form 
 
 const form = document.getElementById('ticketForm');
+
+// gestisco l'evento "submit"
 
 form.addEventListener('submit', function (e) {
 
     e.preventDefault()
 
-    const userKm = Number(document.getElementById('kmInput').value);
-    const userAge = document.getElementById('ageSelect').value;
-    const nameValue = document.getElementById('nameInput').value;
+    // racchiudo i valori degli input nelle costanti
+
+    const userKm = Number(userKmEl.value);
+    const userAge = userAgeEl.value;
+    const nameValue = nameValueEl.value;
 
 
     let fullPrice = 0.21 * userKm;
@@ -24,6 +38,7 @@ form.addEventListener('submit', function (e) {
     let offerType = 'Standard';
 
 
+    // calcolo lo sconto
 
     if (userAge === 'minor') {
         fullPrice = fullPrice * youngDiscount
@@ -32,16 +47,12 @@ form.addEventListener('submit', function (e) {
     } else if (userAge === 'senior') {
         fullPrice = fullPrice * seniorDiscount;
         offerType = 'Senior';
-    } else {
-        fullPrice
     }
 
     console.log('Il prezzo del tuo biglietto è', fullPrice.toFixed(2), '€');
 
-    const ticketType = document.getElementById('ticketType')
-    const newTicket = document.getElementById('generateTicket');
-    const nameTicket = document.getElementById('passengerName');
-    const totalPrice = document.getElementById('ticketPrice');
+    // stampo il biglietto richiamando gli span
+
 
 
     newTicket.classList.remove('d-none');
